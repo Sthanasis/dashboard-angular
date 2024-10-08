5ae9279854b275111d3b446e90abd56a6b0f9978
sthanasis <sthanasis1994@gmail.com>
2024-10-08 16:43:46 +0300

    add tailwind

---
+++ src/theme/mediaQueries.ts
@@ -0,0 +1,5 @@
+export const MediaQueries = {
+  sm: '480px',
+  md: '825px',
+  lg: '1024px',
+} as const
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
+const appPreset = require('./src/theme/tw.preset')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
   plugins: [],
+  presets: [appPreset],
 }
-
